e525dd5e08fe7e868de61d2dcb86f20ab5630cf1
Oleksii Borshch <sergeevi44@gmail.com>
2022-07-02 20:29:45 +0300

    add Statistics section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
+import data from '../components/Statistics/data.json';
+import { Statistics } from './Statistics/Statistics';
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics stats={data} />{' '}
+    </>
   );
 };
