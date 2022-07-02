543300360f3a49bb9592732caa83de3383e7d473
Oleksii Borshch <sergeevi44@gmail.com>
2022-07-02 20:59:47 +0300

    add FriendList section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
-import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
 import data from '../components/Statistics/data.json';
+import friends from '../components/FriendList/friends.json';
+import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 export const App = () => {
   return (
     <>
@@ -12,7 +14,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics stats={data} />{' '}
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,20 @@
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
